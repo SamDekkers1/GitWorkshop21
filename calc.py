a999e6cade6352ee6aa7fa0289ec8cfd31e3e090
SamDekkers <Sam.Dekkers1@monash.edu>
2021-10-11 10:55:26 +1100

    Added calc.py example code

---
+++ calc.py
@@ -0,0 +1,11 @@
+#Example file to use in git workshop
+def compute(expression):
+    values = expression.split(' ')
+    num0 = int(values[0])
+    operator = values[1]
+    num1 = int(values[2])
+    if operator == '+':
+        return num0 + num1
+    else:
+        print('unknown operator!')
+        return 0
